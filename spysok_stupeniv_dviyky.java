fc27f873d99a004a19de8c59f7483cae5e016aec
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 21:05:06 +0200

    Add files via upload

---
+++ spysok_stupeniv_dviyky.java
@@ -0,0 +1,18 @@
+package whilee;
+
+import java.util.Scanner;
+
+public class spysok_stupeniv_dviyky {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = 1;
+		while (b < a ) {
+			System.out.println(b);
+			b *= 2;
+		}
+		
+	}
+
+
+}
