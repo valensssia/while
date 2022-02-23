a2b2824f921dd41bcc0bb0baaf0fde4190358890
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 21:00:12 +0200

    Add files via upload

---
+++ spisok_kvadrativ.java
@@ -0,0 +1,17 @@
+package whilee;
+
+import java.util.Scanner;
+
+public class spisok_kvadrativ {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = 1;
+		while (b * b <= a){
+		    System.out.println(b * b);
+		    b++;
+		}
+	}
+
+
+}
